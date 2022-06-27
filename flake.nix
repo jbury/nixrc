ec8eef820a66b1d61364580f2219ef467b8f4cf0
Jason Bury <jasondougbury@gmail.com>
2022-06-26 21:29:01 -0700

    Initial dead simple setup

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      nonfreepkgs = import "${nixpkgs}" {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pkgs = import "${nixpkgs}" {
+        inherit system;
+        overlays = [ (final: prev:{
+            slack = nonfreepkgs.slack;
+            sublime4 = nonfreepkgs.sublime4;
+        }) ];
+      };
+
+    in {
+      homeConfigurations.jbury = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "jbury";
+  home.homeDirectory = "/home/jbury";
+
+  home.stateVersion = "22.05";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    vim
+    curl
+    zsh
+    git
+    slack
+    ripgrep
+    keepass
+    kubectl
+    kustomize
+    drawio
+    istioctl
+    postgresql
+    pgcenter
+    cloud-sql-proxy
+    tree
+    jq
+    yq-go
+    go_1_18
+    sublime4
+    kubernetes-helm #Helm 3
+    rename
+    shellcheck
+    yamllint
+    ruby_2_7
+  ];
+
+  home.activation = {
+    linkDesktopApplications = {
+      after = [ "writeBoundary" "createXdgUserDirectories" ];
+      before = [ ];
+      data = ''
+        rm -rf ${config.xdg.dataHome}/"applications/home-manager"
+        mkdir -p ${config.xdg.dataHome}/"applications/home-manager"
+        cp -Lr ${config.home.homeDirectory}/.nix-profile/share/applications/* ${config.xdg.dataHome}/"applications/home-manager/"
+      '';
+    };
+  };
+}
