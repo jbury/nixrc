a1ab03960f1a8bde1534f89524e3eddae68c3a12
Jason Bury <jasondougbury@gmail.com>
2022-06-26 23:46:37 -0700

    get applications visible to xdg the dumbest way possible

--- home.nix
+++ home.nix
@@ -13,5 +13,18 @@
     zsh
     git
     slack
+    ripgrep
   ];
+
+  home.activation = {
+    linkDesktopApplications = {
+      after = [ "writeBoundary" "createXdgUserDirectories" ];
+      before = [ ];
+      data = ''
+        rm -rf ${config.xdg.dataHome}/"applications/home-manager"
+        mkdir -p ${config.xdg.dataHome}/"applications/home-manager"
+        cp -Lr ${config.home.homeDirectory}/.nix-profile/share/applications/* ${config.xdg.dataHome}/"applications/home-manager/"
+      '';
+    };
+  };
 }
