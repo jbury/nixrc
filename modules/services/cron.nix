52bb857f729d130d94339601c33a1b763a4c85dd
Jason Bury <jason.bury@flexe.com>
2023-03-28 12:07:23 -0700

    Add cron and my silly screenshot cronjob

---
+++ modules/services/cron.nix
@@ -0,0 +1,20 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.cron;
+    configDir = config.dotfiles.configDir;
+in {
+  options.modules.services.cron = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.cron = {
+      enable = true;
+      systemCronJobs = [
+        "*/15 * * * *      jbury    work_screenshot"
+      ];
+    };
+  };
+}
