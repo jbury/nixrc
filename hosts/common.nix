2586a3e39457f9cc50387f4051bfeaa8acf16604
Jason Bury <jason.bury@flexe.com>
2024-01-11 10:42:34 -0800

    start on the home-manager journey

---
+++ hosts/common.nix
@@ -0,0 +1,21 @@
+{ lib, ... }:
+
+let inherit (lib) mkDefault;
+in {
+  time.timeZone = mkDefault "America/Los_Angeles";
+
+  i18n = {
+    defaultLocale = mkDefault "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+}
