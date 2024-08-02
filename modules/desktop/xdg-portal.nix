bab9116d8323b0cc765a4dfb30033c261b5ef2e4
Jason Bury <jason@knock.app>
2024-08-02 15:42:33 -0700

    Add xdg-desktop-portals to the mix to get screensharing in zoom working.

--- modules/desktop/xdg-portal.nix
+++ modules/desktop/xdg-portal.nix
@@ -1,18 +1,9 @@
-{ config, pkgs, ... }: {
-  home-manager.users.${config.user.name}.xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-wlr
-    ];
-    xdgOpenUsePortal = true;
+{ config, ... }: {
+  config = {
+    xdg.portal = {
+      enable = true;
+      config.common.default = "*";
+      wlr.enable = true;
+    };
   };
-
-  # config = {
-  #   xdg.portal = {
-  #     enable = true;
-  #     wlr.enable = true;
-
-  #   };
-  # };
 }
