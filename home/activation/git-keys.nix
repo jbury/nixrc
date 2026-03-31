9772d6ca49c970c7a36989bc012bc908b14ad6c4
Jason Bury <2317537+jbury@users.noreply.github.com>
2026-03-30 18:00:24 -0700

    Fix git-keys.nix - I dunno how I got that so wrong

--- home/activation/git-keys.nix
+++ home/activation/git-keys.nix
@@ -23,6 +23,8 @@ in {
 					pub  = "${homeSettings.homeDirectory}/.ssh/default_git.pub";
 					priv = "${homeSettings.homeDirectory}/.ssh/default_git";
 				};
+
+				ssh-keygen = "${pkgs.openssh}/bin/ssh-keygen -t ed25519 -N \"\"";
 			in ''
 # Make sure ~/.ssh exists
 if [ ! -d "${homeSettings.homeDirectory}/.ssh" ]; then
@@ -34,25 +36,21 @@ chmod 700 "${homeSettings.homeDirectory}/.ssh"
 
 # Handle personal key creation
 if [ ! -f "${personalGithubKeys.priv}" ]; then
-	${pkgs.openssl}/bin/ssh-keygen -t ed25519 -f "${personalGithubKeys.priv}" -N "${homeSettings.userName}" -C "${personalEmail}"
+	${ssh-keygen} -f "${personalGithubKeys.priv}" -C "${personalEmail}"
 	chmod 600 "${personalGithubKeys.priv}"
 	chmod 644 "${personalGithubKeys.pub}"
-	${pkgs.openssl}/bin/ssh-key add "${personalGithubKeys.pub}" --type authentication --title "${homeSettings.hostname}-personal"
-	${pkgs.openssl}/bin/ssh-key add "${personalGithubKeys.pub}" --type signing --title "${homeSettings.hostname}-personal"
 fi
 
 # Handle default key creation or symlink
 if [ ! -f "${defaultGitKeys.priv}" ]; then
 	# No overriding primary homeSettings email, so this isn't a work machine
 	if [ "${homeSettings.email}" == "${personalEmail}" ]; then
-		ln -s "${personalGithubKeys.pub}" "${defaultGitKeys.pub}"
-		ln -s "${personalGithubKeys.priv}" "${defaultGitKeys.priv}"
+		ln -sf "${personalGithubKeys.pub}" "${defaultGitKeys.pub}"
+		ln -sf "${personalGithubKeys.priv}" "${defaultGitKeys.priv}"
 	else
-		${pkgs.openssl}/bin/ssh-keygen -t ed25519 -f "${defaultGitKeys.priv}" -N "${homeSettings.userName}" -C "${homeSettings.email}"
+		${ssh-keygen} -f "${defaultGitKeys.priv}" -C "${homeSettings.email}"
 		chmod 600 "${defaultGitKeys.priv}"
 		chmod 644 "${defaultGitKeys.pub}"
-		${pkgs.openssl}/bin/ssh-key add "${defaultGitKeys.pub}" --type authentication --title "${homeSettings.hostname}-default"
-		${pkgs.openssl}/bin/ssh-key add "${defaultGitKeys.pub}" --type signing --title "${homeSettings.hostname}-default"
 	fi
 fi
 			''
