8bed40ee1028ed88b9c4570b61e52f4eed77144c
Jason Bury <jasondougbury@gmail.com>
2026-08-29 20:15:16 -0700

    signal-desktop, not just signal

--- home/desktop/wayland/sway/keybindings.nix
+++ home/desktop/wayland/sway/keybindings.nix
@@ -27,7 +27,7 @@ in {
           "${super}+l" = "exec swaylock -Fe -c000000";
           "${super}+f" = "exec firefox";
           "${super}+k" = "exec keepassxc";
-          "${super}+s" = "signal";
+          "${super}+s" = "signal-desktop";
 
           "${super}+q" = "kill";
 					#"${super}+space" = "exec $DOTFILES/bin/rofi/appmenu";
